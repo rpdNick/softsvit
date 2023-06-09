d71e9e212f3f9d12836bdd52d4c4c51803aaffa2
rpdNick <nickyovchenko@gmail.com>
2023-06-09 16:33:07 +0300

    sphere animation on transform

--- js/sphere-animation.js
+++ js/sphere-animation.js
@@ -1,8 +1,6 @@
-// const deskScrollBottom = document.getElementById("position-to-scroll-bottom");
-// const drskPositionToScrollTop = document.querySelector(".main_screen_wrap");
-
 const desktopSphereBox = document.querySelector("#sphereDesktop");
 const mobileSphareBox = document.querySelector("#sphereMobile");
+
 let currentDesktopAnimationState = 0;
 
 function desktopSphereAnimation() {
@@ -11,12 +9,7 @@ function desktopSphereAnimation() {
 }
 
 function desktopAnimation() {
-  const startPosition =
-    document.getElementById("scroll-first-position").getBoundingClientRect()
-      .top + window.scrollY;
-  const animateToTop =
-    document.getElementById("about_us").getBoundingClientRect().top +
-    window.scrollY;
+  const startPosition = document.getElementById("scroll-first-position").getBoundingClientRect().top + window.scrollY;
 
   mobileSphareBox.style.display = "none";
   desktopSphereBox.style.display = "block";
@@ -24,35 +17,36 @@ function desktopAnimation() {
   let timeout;
 
   window.addEventListener("scroll", function () {
-    var currentScroll =
-      window.pageYOffset || document.documentElement.scrollTop;
-    var startPosition = 500; // Начальная позиция, от которой должна начинаться анимация
+    var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
     var direction = 1; // Направление анимации (1 - вниз, -1 - вверх)
 
     params = {
       start: [
         {
-          width: "674",
+          scale: "1",
           right: "100",
-          rotation: "rotate(0deg)",
+          transformation: "0",
           opacity: "1",
         },
       ],
 
       second: [
         {
-          width: "848",
-          right: "-250",
-          rotation: "rotate(-90deg)",
+          scale: "1.3",
+          transformationY: "70",
+          transformationX: "180",
+          rotation: "-100",
           opacity: "1",
         },
       ],
 
       end: [
         {
-          width: "514",
-          right: "80",
-          rotation: "rotate(-150deg)",
+          scale: "0.8",
+          transformationY: "180",
+          transformationX: "-200",
+          rotation: "-150",
           opacity: "0.2",
         },
       ],
@@ -60,33 +54,35 @@ function desktopAnimation() {
 
     if (currentScroll > startPosition) {
       if (currentDesktopAnimationState !== 1 || direction !== 1) {
-        desktopSphereBox.style.width = params.second[0].width + "px";
-        desktopSphereBox.style.top = startPosition * 1.6 + "px";
-        desktopSphereBox.style.right = params.second[0].right + "px";
-        desktopSphereBox.style.transform = params.second[0].rotation;
+        desktopSphereBox.style.scale = params.second[0].scale;
+        desktopSphereBox.style.transform =
+          "translate(" +
+          params.second[0].transformationX +
+          "px, " +
+          params.second[0].transformationY +
+          "%) rotate(" +
+          params.second[0].rotation +
+          "deg)";
         desktopSphereBox.style.opacity = params.second[0].opacity;
 
         setTimeout(function () {
-          desktopSphereBox.style.width = params.end[0].width + "px";
-          desktopSphereBox.style.top = startPosition * 1.7 + "px";
-          desktopSphereBox.style.right = params.end[0].right + "%";
-          desktopSphereBox.style.transform = params.end[0].rotation;
+          desktopSphereBox.style.scale = params.end[0].scale;
+          desktopSphereBox.style.transform = "translate(calc(" + params.end[0].transformationX + "%), " + params.end[0].transformationY + "%)";
           desktopSphereBox.style.opacity = params.end[0].opacity;
+
           currentDesktopAnimationState = 1;
 
           // animation direction
           direction = -1;
-        }, 1000);
+        }, 600);
       }
     } else if (currentScroll < startPosition) {
       if (currentDesktopAnimationState !== 0 || direction !== -1) {
-        desktopSphereBox.style.width = params.start[0].width + "px";
-        desktopSphereBox.style.top = 84 + "px";
-        desktopSphereBox.style.right = params.start[0].right + "px";
-        desktopSphereBox.style.transform = params.start[0].rotation;
+        desktopSphereBox.style.scale = params.start[0].scale;
+        desktopSphereBox.style.transform = "translate(" + params.start[0].transformation + "px)";
         desktopSphereBox.style.opacity = params.start[0].opacity;
-        currentDesktopAnimationState = 0;
 
+        currentDesktopAnimationState = 0;
         // animation direction
         direction = 1;
       }
