57e1383f50a554cb6860415477e551fb895d75e2
rpdNick <nickyovchenko@gmail.com>
2023-06-20 16:23:56 +0300

    ellipse style on pc

--- js/sphere-desktop.js
+++ js/sphere-desktop.js
@@ -50,7 +50,7 @@ let earth = document.getElementById("sphereDesktop").animate(
   {
     fill: "forwards",
     duration: 2100,
-    easing: 'ease-in-out',
+    easing: 'ease-out',
     // easing: "cubic-bezier(0.9,0.9,0.5,0.5)",
   }
 );
