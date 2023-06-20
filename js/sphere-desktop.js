bd01baee5d13a2f0e7f2aa386a412dec44efba1f
rpdNick <nickyovchenko@gmail.com>
2023-06-20 11:05:23 +0300

    earth duration

--- js/sphere-desktop.js
+++ js/sphere-desktop.js
@@ -49,7 +49,7 @@ let earth = document.getElementById("sphereDesktop").animate(
   ],
   {
     fill: "forwards",
-    duration: 1700,
+    duration: 2200,
     easing: 'ease-in-out',
     // easing: "cubic-bezier(0.9,0.9,0.5,0.5)",
   }
